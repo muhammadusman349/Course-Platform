017f76fc71f75347774e4779615bd7c8a5c0dc7b
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-10-02 13:24:50 +0500

    3 - Add image field and makemigration and migrate with Django

--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
-    EMAIL_REQUIRED = "email_required", "Email required"
+    EMAIL_REQUIRED = "email", "Email required"
 
 
 class PublishStatus(models.TextChoices):
@@ -14,12 +14,16 @@ class PublishStatus(models.TextChoices):
     DRAFT = "draft", "Draft"
 
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-
-    access = models.CharField(max_length=10, choices=AccessRequirement.choices,
-                              default=AccessRequirement.ANYONE)
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    access = models.CharField(max_length=5, choices=AccessRequirement.choices,
+                              default=AccessRequirement.EMAIL_REQUIRED)
     status = models.CharField(max_length=10, choices=PublishStatus.choices,
                               default=PublishStatus.DRAFT)
 
