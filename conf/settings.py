85c48456f2c8dd6be71c03a0095c66ee7cf285be
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-10-02 15:45:35 +0500

    4 - Add lesson model add helpers and setup cloudinary

--- conf/settings.py
+++ conf/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOCAL_CDN =BASE_DIR.parent / "local-cdn"
+MEDIA_ROOT = LOCAL_CDN / "media"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,10 @@
+import helpers
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+helpers.cloudinary_init()
 
 
 class AccessRequirement(models.TextChoices):
@@ -21,7 +25,8 @@ def handle_upload(instance, filename):
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField("image", null=True)
     access = models.CharField(max_length=5, choices=AccessRequirement.choices,
                               default=AccessRequirement.EMAIL_REQUIRED)
     status = models.CharField(max_length=10, choices=PublishStatus.choices,
@@ -30,3 +35,45 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+
+    @property
+    def image_admin_url(self):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": 200
+        }
+        url = self.image.build_url(**image_options)
+        return url
+
+    def get_image_thumbnail(self, as_html=False, width=500):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            # CloudinaryImage(str(self.image)).image(**image_options)
+            return self.image.image(**image_options)
+        # CloudinaryImage(str(self.image)).image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            # CloudinaryImage(str(self.image)).image(**image_options)
+            return self.image.image(**image_options)
+        # CloudinaryImage(str(self.image)).image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
