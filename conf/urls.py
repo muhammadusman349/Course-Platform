f144b992d38078f05696013bec143b4b231a97d8
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-10-03 13:12:03 +0500

    8- Rendering the video player for users

--- conf/urls.py
+++ conf/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
+from .import views
 
 urlpatterns = [
+    path("", views.home_view),
     path("admin/", admin.site.urls),
+    path("courses/", include("courses.urls")),
 ]
 
 if settings.DEBUG:
